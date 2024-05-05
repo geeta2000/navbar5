746b6ee2c6e24fade177319ca06add483f638436
geeta2000 <giranchipatel22@gmail.com>
2024-05-05 22:38:10 +0530

    tailwind setup

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div>this is home page</div>;
+  return <div className="bg-red-500 p-2">this is home page</div>;
 };
 
 export default Home;
